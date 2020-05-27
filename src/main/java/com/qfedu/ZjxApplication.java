4cce9cc03b005252af24b6f597201bfd3976aa98
zjx <912660661@qq.com>
2020-05-27 20:17:58 +0800

    啦啦啦啦

---
+++ src/main/java/com/qfedu/ZjxApplication.java
@@ -0,0 +1,15 @@
+package com.qfedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
+
+
+@SpringBootApplication
+public class ZjxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjxApplication.class, args);
+    }
+
+}
